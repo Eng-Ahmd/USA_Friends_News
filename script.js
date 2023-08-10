bca76da0b05398a0dd2c4b5d8ab7ebedd5abdbd3
Ahmed Abdelrahman <37957862+Eng-Ahmd@users.noreply.github.com>
2023-08-10 09:20:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,43 @@
-// Create a slider
-let slider = document.createElement('input');
-slider.type = 'range';
-slider.id = 'dateSlider';
-slider.style.width = '50%';
-slider.style.display = 'block';
-document.body.insertBefore(slider, document.getElementById('output'));
-
-// Create an element to display the current date
-let currentDate = document.createElement('p');
-currentDate.id = 'currentDate';
-document.body.insertBefore(currentDate, document.getElementById('output'));
-
+// Fetch the dates first
 fetch('dates.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-    })
+    .then(response => response.json())
     .then(dates => {
-        dates.sort(); // Ensure the dates are sorted in ascending order
-
-        slider.min = 0;
-        slider.max = dates.length - 1;
-        slider.value = dates.length - 1;
-        slider.step = 1;
-
-        // Display the current date
-        currentDate.textContent = `Date: ${dates[slider.value]}`;
-
-        let fetchPromises = dates.map(date => {
-            return fetch(`output-${date}.txt`).then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                return response.text();
-            }).catch(() => null);
+        // Convert the dates into a readable format
+        let readableDates = dates.map(date => {
+            let parts = date.split('_');
+            return `2023-${parts[0]}-${parts[1]}`; 
         });
 
-        let outputData = {};
-        let lastSuccessfulData = '';
+        // The last date will be the default date
+        let defaultDate = readableDates[readableDates.length - 1];
 
-        Promise.all(fetchPromises).then(data => {
-            for (let i = 0; i < dates.length; i++) {
-                if (data[i] !== null) {
-                    lastSuccessfulData = data[i];
-                }
-                outputData[dates[i]] = lastSuccessfulData;
+        // Set the default date
+        document.getElementById('datePicker').value = defaultDate;
+
+        // Load the data for the default date
+        loadDateData(dates[dates.length - 1]);
+
+        // Attach an event listener to the date picker
+        document.getElementById('datePicker').addEventListener('change', function() {
+            let selectedDate = this.value;
+            let index = readableDates.indexOf(selectedDate);
+            if (index !== -1) {
+                loadDateData(dates[index]);
             }
-
-            slider.addEventListener('input', function() {
-                document.getElementById('output').textContent = outputData[dates[this.value]];
-                currentDate.textContent = `Date: ${dates[this.value]}`;
-            });
-
-            document.getElementById('output').textContent = outputData[dates[dates.length - 1]];
         });
+
     })
     .catch(e => {
         console.log('There has been a problem with your fetch operation: ' + e.message);
     });
+
+function loadDateData(date) {
+    fetch(`output-${date}.txt`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('output').textContent = data;
+        })
+        .catch(error => {
+            console.log('Error loading date data: ' + error.message);
+        });
+}
